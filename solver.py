02a0afbae5fbda703b5a3f2a7b3abe319da9d5f5
phmpm <mike.mercer87@gmail.com>
2018-11-02 13:06:12 +0000

    Mike commit. Synchronise directories.

--- solver.py
+++ solver.py
@@ -11,14 +11,17 @@ class Diffusion():
         self.R = 1e-05 # radius of particle
         self.C_max = 12000 # Cutoff conditions
         self.C_0 = 9500
-        self.J_0 = 9.5e-3
+        self.J_0 = 9.5e-6
         self.D_s = 1e-14 # Diffusion coefficient
         self.deltat = 1 # Time interval.
         self.deltar = (1/50) * self.R # radius_interval
         self.no_rpoints = int(self.deltar / self.R)
         self.T_max = 2*3600 # End time
-        self.t_array = np.linspace(0, self.T_max, self.T_max) # Time array
-        self.r_array = np.linspace(0, self.R, 51) # radius array
+        self.no_tpoints = int(self.deltat / self.T_max)
+        print(self.no_rpoints)
+        print(self.no_tpoints)
+        self.t_array = np.linspace(0, self.T_max, self.no_tpoints) # Time array
+        self.r_array = np.linspace(0, self.R, self.no_rpoints) # radius array
         self.C_array = np.ones((self.t_array.size,self.r_array.size))*self.C_0 # Concentration array, intialised.
 #        self.j_array = np.zeros(self.T_max)
         
@@ -38,7 +41,7 @@ class Diffusion():
             self.boundary_conditions()
             for k in range(0,self.no_rpoints):
                 self.numerator = self.C_array[i,k+1] - 2 * self.C_array[i,k] + self.C_array[i,k - 1]
-                self.C_array[i+1,k] = self.C_array[i,k] + self.deltat * self.D * (1 / self.r_array[k]) * (self.C_array[i,k+1] - self.C_array[i,k-1]) / self.deltar + self.numerator / (self.deltar) ** 2 
+                self.C_array[i+1,k] = self.C_array[i,k] + self.deltat * self.D * ((1 / self.r_array[k]) * (self.C_array[i,k+1] - self.C_array[i,k-1]) / self.deltar + self.numerator / (self.deltar) ** 2) 
                                                     
 
 if __name__ == '__main__':
